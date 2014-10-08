7aad26628d4fef2883e237eaaa6483dc9eef970e
msstudentt <jpty9@mst.edu>
2014-10-08 12:45:01 -0500

    Adding base brython code for references

---
+++ brython/server.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+
+
+import sys
+import os
+
+
+try:
+    os.nice(19)
+except Exception as error:
+    print(error)
+if sys.version < '3':
+    import BaseHTTPServer as server
+    from CGIHTTPServer import CGIHTTPRequestHandler
+else:
+    import http.server as server
+    from http.server import CGIHTTPRequestHandler
+
+
+server_address, handler = ('', 8000), CGIHTTPRequestHandler
+httpd = server.HTTPServer(server_address, handler)
+print(("Server running on port http://localhost:{}/site.".format(server_address[1])))
+httpd.serve_forever()
---
+++ brython/src/brython_builtins.js
@@ -0,0 +1,70 @@
+var __BRYTHON__=__BRYTHON__ || {}  // global object with brython built-ins
+
+;(function($B) {
+
+if ($B.isa_web_worker==true) {
+  // we need to emulate a window and document variables/functions for
+  // web workers, since they don't exists. (this is much better than,
+  // having a bunch of tests throughout code, making the code more complex) 
+
+  window = {}
+  window.XMLHttpRequest = XMLHttpRequest 
+  window.navigator={}
+  window.navigator.userLanguage=window.navigator.language="fixme"
+
+  window.clearTimeout=function(timer) {clearTimeout(timer)}
+}
+
+// Python __builtins__
+$B.builtins = {
+    __repr__:function(){return "<module 'builtins>'"},
+    __str__:function(){return "<module 'builtins'>"},    
+}
+
+$B.__getattr__ = function(attr){return this[attr]}
+$B.__setattr__ = function(attr,value){
+    // limited to some attributes
+    if(['debug'].indexOf(attr)>-1){$B[attr]=value}
+    else{throw $B.builtins.AttributeError('__BRYTHON__ object has no attribute '+attr)}
+}
+
+// system language ( _not_ the one set in browser settings)
+// cf http://stackoverflow.com/questions/1043339/javascript-for-detecting-browser-language-preference
+$B.language = window.navigator.userLanguage || window.navigator.language
+
+// document charset ; defaults to "utf-8"
+$B.charset = document.characterSet || document.inputEncoding || "utf-8"
+
+$B.date = function(){
+    if(arguments.length===0) return $B.JSObject(new Date())
+    if(arguments.length===1) return $B.JSObject(new Date(arguments[0]))
+    if(arguments.length===7) return $B.JSObject(new Date(arguments[0],
+        arguments[1]-1,arguments[2],arguments[3],
+        arguments[4],arguments[5],arguments[6]))
+}
+
+$B.has_local_storage = typeof(Storage)!=="undefined"
+if($B.has_local_storage){
+    // add attributes local_storage and session_storage
+    $B.local_storage = localStorage
+    $B.session_storage = sessionStorage
+}
+
+$B._indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB
+$B.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction
+$B.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange
+
+$B.has_indexedDB = typeof($B._indexedDB) !== "undefined"
+if ($B.has_indexedDB) {
+   $B.indexedDB = function() {return $B.JSObject($B._indexedDB)}
+}
+
+$B.re = function(pattern,flags){return $B.JSObject(new RegExp(pattern,flags))}
+$B.has_json = typeof(JSON)!=="undefined"
+
+$B.has_websocket = (function(){
+    try{var x=window.WebSocket;return x!==undefined}
+    catch(err){return false}
+})
+
+})(__BRYTHON__)
