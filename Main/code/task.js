078a0ff6bec46c2d0bfdcd6df63711f8aa4200e1
mststudent <jpty9@mst.edu>
2014-11-20 18:47:23 -0600

    Update Minor save issue but shouldn't be to hard to fix

--- Main/code/task.js
+++ Main/code/task.js
@@ -1,22 +1,16 @@
-// Programmer: Doug Melville 
+// Programmers: Doug Melville , Joseph Pantoga, Sean Dickey 
 // Date: Nov 15, 2014
 // File: index.js
 // Purpose: a much cleaner implementation of our task manager using d3 fully
 
-//Script for the "Save" button
-var saveAddAllData = function(){
-  if($("#jsonArea").text() === ''){
-  addAllData();
-  }
-}
-
 // files
-const FILE_JSON = "default.json";
+const FILE_JSON = "demo.json";
 
 // classes
 const CLASS_Task = "taskRect";
 const CLASS_TXT_Name = "taskName";
 const CLASS_TXT_Description = "taskDescription";
+const CLASS_PERCENT_DONE = "percentDescription"
 
 // ids
 const ID_PRFX_Task = "tsk";
@@ -28,6 +22,7 @@ const ID_BTN_Edit = "BTN_edit";
 const ID_BTN_Delete = "BTN_delete";
 
 // padding
+const NUM_Buttons = 4;
 const PAD_Menu_X = 25;
 const PAD_Menu_Y = 25;
 const PAD_Text_X = 50;
@@ -44,29 +39,45 @@ const HEIGHT_Canvas = 700;
 const HEIGHT_Buttons = 50;
 
 // Test
-const TEXT_SIZE_Labels = 32;
+const TEXT_SIZE_Labels = 48;
+
+// Default Rect Color
+const DEF_COLOR = "blue"
 
 var taskTree = null;
 var canvas = null;
 var selectedData = null;
 var addColor;
+var percentDone;
+var percentDoneSlider;
 var maxId = 5;
+var zoomedTo = null;
+var formVisible = false;
 
-var colorWheel;
+var colorWheel = null;
+
+//Script for the "Save" button
+var saveAddAllData = function () {
+    if ($("#jsonArea").text() === '') {
+        addAllData();
+    }
+}
+
+function addSlider(startingPercent){
+        var elem = document.querySelector("#slider");
+        percentDoneSlider = new Powerange(elem,{ 
+		min: 0, 
+		max: 100, 
+		start: startingPercent,
+		callback: function(){
+			$("#slider").val() != '100'? $("#percentage").html($("#slider").val()+'% Completed'):$("#percentage").html('Task Completed')
+			}
+		});
+}
 
 function addWheel(){
- var acw = Raphael.colorwheel($("#colorWheel")[0],100);
- var temp = acw.color;
-
- colorWheel = acw;
-
- var setColor = function(){
-     addColor = temp();
-     console.log("addColor: ",addColor);
- }
-
- acw.ondrag(setColor,setColor);
- acw.input($("#wheelText")[0]);
+	colorWheel = Raphael.colorwheel($("#colorWheel")[0], 100);
+	colorWheel.input($("#wheelText")[0]);
 }
 
 var addTabs = function(numTabs){
@@ -76,13 +87,26 @@ var addTabs = function(numTabs){
 }
 
 function updateTextBox(data, tabs) {
+	// this prints nicely formatted JSON! :)
     addTabs(tabs);
     $("#jsonArea").append('\"id\":'+data.id+',\n');
     addTabs(tabs);
-    $("#jsonArea").append('\"name\": "'+data.name+'",\n');
+    $("#jsonArea").append('\"name\":\"'+data.name+'\",\n');
+    if(data.description){
+      addTabs(tabs);
+      $("#jsonArea").append('\"description\":\"'+data.description+'\",\n');
+    }
+    if(data.percent){
+      addTabs(tabs);
+      $("#jsonArea").append('\"percent\": '+data.percent+',\n');
+    }
+    if(data.color){
+	addTabs(tabs);
+	$("#jsonArea").append('\"color\": \"'+data.color+'\",\n');
+    }
     addTabs(tabs);
     $("#jsonArea").append('\"children\":[');
-    if (data.children) {
+    if (data.children && data.children.length > 0) {
         $("#jsonArea").append('\n');
         data.children.forEach(function (child, index) {
             addTabs(tabs);
@@ -108,47 +132,59 @@ function addAllData() {
 }
 
 function addButtons(){
-	var buttonWidth = (WIDTH_Canvas / 3) - (2 * PAD_Buttons);
+	var buttonWidth = ((WIDTH_Canvas-((NUM_Buttons - 1) * PAD_Buttons)) / NUM_Buttons);
 
-	d3.select("#taskArea").append("button")
+    d3.select("body").append("button")
+.attr("id", "goHome")
+.style("position", "absolute")
+.style("left", 3*buttonWidth + 4*PAD_Buttons + "px")
+.style("top", "0px")
+.style("width", buttonWidth + "px")
+.style("height", HEIGHT_Buttons + "px")
+.text("Home")
+.on("click", function () { window.location.replace('http://localhost:5000') });
+
+	d3.select("body").append("button")
 	.attr("id", ID_BTN_Add)
-	.style("position", "relative")
-	.style("left", "0px")
+	.style("position", "absolute")
+	.style("left", PAD_Buttons+"px")
 	.style("top", "0px")
 	.style("width", buttonWidth + "px")
 	.style("height", HEIGHT_Buttons + "px")
 	.text("add")
 	.on("click", function(){myAdd(selectedData);});
 
-	d3.select("#taskArea").append("button")
+	d3.select("body").append("button")
 	.attr("id", ID_BTN_Edit)
-	.style("position", "relative")
-	.style("left", 3*PAD_Buttons+"px")
+	.style("position", "absolute")
+	.style("left", buttonWidth + 2*PAD_Buttons + "px")
 	.style("top", "0px")
 	.style("width", buttonWidth + "px")
 	.style("height", HEIGHT_Buttons + "px")
 	.text("edit")
 	.on("click", function(){myEdit(selectedData);});
 
-	d3.select("#taskArea").append("button")
+	d3.select("body").append("button")
 	.attr("id", ID_BTN_Delete)
-	.style("position", "relative")
-	.style("left", 6*PAD_Buttons+"px")
+	.style("position", "absolute")
+	.style("left", 2*buttonWidth + 3*PAD_Buttons+"px")
 	.style("top", "0px")
 	.style("width", buttonWidth + "px")
 	.style("height", HEIGHT_Buttons + "px")
 	.text("delete")
 	.on("click", function () { myDelete(selectedData); });
 
-	d3.select("#jsonSection").append("button")
+/* Uncomment if you need to debug json printing.
+	d3.select("body").append("button")
 	.attr("id", "giveMeJson")
-	.style("position", "relative")
-	.style("left", 6 * PAD_Buttons + "px")
+	.style("position", "absolute")
+	.style("left", 3*buttonWidth + 4*PAD_Buttons + "px")
 	.style("top", "0px")
 	.style("width", buttonWidth + "px")
 	.style("height", HEIGHT_Buttons + "px")
 	.text("Print Json")
 	.on("click", function () { addAllData(); });
+*/
 }
 
 function updateSelectedData(d){
@@ -162,15 +198,23 @@ function updateSelectedData(d){
 	.attr("opacity", "0.7");
 }
 
-function zoomTo(d, duration){
+function zoomTo(d, duration, heightMod){
+	// added hacky heightMod variable to allow us to zoom to different heights
+	// TODO: Fix this so it isn't awful
+	var heightDomain = 1;
+	if(heightMod){
+		heightDomain = d.y + d.dy;
+	}
+
 	// remove the menu if we're giving up on editing for some reason
 	var menu = d3.select("#" + ID_Menu);
 	if(menu){
 		menu.remove();
+		formVisible = false;
 	}
 
 	x.domain([d.x, d.x + d.dx]);
-	y.domain([d.y, 1]).range([d.y ? 50 : 0, HEIGHT_Canvas]);
+	y.domain([d.y, heightDomain]).range([d.y ? 50 : 0, HEIGHT_Canvas]);
 
 	rects = d3.select("#"+ID_Canvas).selectAll("rect");
 	rects.transition()
@@ -197,16 +241,27 @@ function zoomTo(d, duration){
 	.attr("x", function(d) { return x(d.x) + PAD_Text_X; })
 	.attr("y", function(d) { return y(d.y) + 2*PAD_Text_Y; })
 	.attr("font-size", function(d){return ((x(d.x + d.dx) - x(d.x))/WIDTH_Canvas)*TEXT_SIZE_Labels; });
+
+        d3.selectAll("."+CLASS_PERCENT_DONE).transition().duration(duration)
+        .attr("user-select", "none")
+        .attr("x_old", function(d) { return d.x; })
+        .attr("y_old", function(d) { return d.y; })
+        .attr("x", function(d) { return x(d.x) + PAD_Text_X; })
+        .attr("y", function(d) { return y(d.y) + 4*PAD_Text_Y; })
+        .attr("font-size", function(d){return ((x(d.x + d.dx) - x(d.x))/WIDTH_Canvas)*TEXT_SIZE_Labels; });
+
+	zoomedTo = d;
 }
 
 function onDblclick_TaskRect(d){
-	zoomTo(d, 250);
+	zoomTo(d, 250, false);
+	updateSelectedData(d);
 }
 
 // for partitioning
 var x = d3.scale.linear().range([0, WIDTH_Canvas]);
 var y = d3.scale.linear().range([0, HEIGHT_Canvas]);
-var color = d3.scale.category20c();
+// var color = d3.scale.category20c();
 var partition = d3.layout.partition()
 	.children(function(d) { return d.children; })
 	.value(function(d) { return 1; })
@@ -226,8 +281,10 @@ function drawTree(c, t){
     .attr("x", function(d){ return x(d.x); })
     .attr("y", function(d){ return y(d.y); })
     .attr("width", function(d) { return x(d.dx); })
-    .attr("height", function(d) { return y(d.dy); })
-    .attr("fill", function(d) { return color(d.color ? d.color : "blue"); });
+    .attr("height", function(d) { return y(d.dy); });
+
+    // TODO: Figure out why this won't transition anymore?
+    s.select("rect").attr("fill", function(d) { return d.color ? d.color : DEF_COLOR });
 
     s.select("." + CLASS_TXT_Name).transition().duration(DUR_Update)
     .attr("x", function(d) { return x(d.x) + PAD_Text_X; })
@@ -241,6 +298,13 @@ function drawTree(c, t){
     .attr("font-size", function(d){return d.dx*TEXT_SIZE_Labels; })
     .text(function(d){ return d.description; });
 
+    s.select("." + CLASS_PERCENT_DONE).transition().duration(DUR_Update)
+    .attr("x", function(d) { return x(d.x) + PAD_Text_X; })
+        .attr("y", function(d) { return y(d.y) + 4*PAD_Text_Y; })
+    .attr("font-size", function(d){return d.dx*TEXT_SIZE_Labels; })
+    .text(function(d){ 
+     return d.percent ? d.percent+'% Completed' : "0% Completed";
+     });
 
 	// enter
 	g = s.enter().append("g")
@@ -254,7 +318,7 @@ function drawTree(c, t){
     .attr("y", function(d){ return y(d.y); })
     .attr("width", function(d) { return x(d.dx); })
     .attr("height", function(d) { return y(d.dy); })
-    .attr("fill", function(d) { return color(d.color ? d.color : "blue"); });
+    .attr("fill", function(d) { return (d.color ? d.color : DEF_COLOR); });
 
     g.append("text")
     .attr("class", CLASS_TXT_Name)
@@ -272,8 +336,20 @@ function drawTree(c, t){
     .text(function(d){ return d.description; })
     .attr("pointer-events", "none");
 
+    g.append("text")
+    .attr("class", CLASS_PERCENT_DONE)
+    .attr("x", function(d) { return x(d.x) + PAD_Text_X; })
+    .attr("y", function(d) { return y(d.y) + 4*PAD_Text_Y; })
+    .attr("font-size", function(d){return d.dx*TEXT_SIZE_Labels; })
+    .text(function(d){
+	  return d.percent ? d.percent+'% Completed' : "0% Completed";
+	 })
+    .attr("pointer-events", "none");
+
+
     // exit
     s.exit().remove();
+    addAllData();
 }
 
 function newTask(){
@@ -287,22 +363,13 @@ function newTask(){
 }
 
 function updateFromForm(d){
-        var newName = document.getElementById("titleInput").value;
-        var newDetails = document.getElementById("desTextArea").value;
-
-        newName ? d.name = newName : console.log("Name didn't change.");
-        newDetails ? d.description = newDetails: console.log("Details didn't change.");
-        addColor ? d3.select("#" + ID_PRFX_Task + selectedData.id).select("rect").attr("fill",addColor.toString()) : console.log("Color didn't change.");
-        if(newName && newDetails && addColor){
-		console.log("Something Changed");
-	}else{
-		console.log("Nothing Changed");
-		d3.select("#menu").remove();
-		return
-	}
-	drawTree(canvas, partition(taskTree));
-        d3.select("#menu").remove(); 
-	zoomTo(d, DUR_Update);
+    d.name = document.getElementById("titleInput").value;
+    d.description = document.getElementById("desTextArea").value;
+    d.percent = $("#slider").val();
+    d.color = colorWheel.color();
+    drawTree(canvas, partition(taskTree));
+    zoomTo(d, DUR_Update, false);
+    d3.selectAll("text").attr("display","");
 }
 
 function showForm(d){
@@ -320,14 +387,14 @@ function showForm(d){
 	.attr("id", "titleInput")
 	.attr("type", "text")
 	.attr("size", 14)
-	.attr("placeholder", d.name)
+	.attr("value", d.name)
 	.style("font-size", "32px");
 
 	form.append("textarea")
 	.attr("id", "desTextArea")
 	.attr("rows", 3)
 	.attr("cols", 13)
-	.attr("placeholder", d.description)
+	.attr("value", d.description)
 	.attr("wrap", "hard")
 	.style("font-size", "32px");
 
@@ -336,21 +403,43 @@ function showForm(d){
 	.attr("value", "OK")
 	.on("click", updateFromForm, d);
 
-        form.append("div")
-        .attr("id", "colorWheel")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("position","absolute");
+    form.append("div")
+    .attr("id", "colorWheel")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("position","absolute");
 
-        form.append("input")
-        .attr("size","7")
-        .attr("type","text")
-        .attr("id","wheelText")
-        .attr("x", 425)
-        .attr("y", 100)
-        .attr("position","absolute");
-        addWheel();
-        colorWheel.color(color(d3.select("#" + ID_PRFX_Task + selectedData.id).select("rect")));
+    form.append("input")
+    .attr("size","7")
+    .attr("type","text")
+    .attr("id","wheelText")
+    .attr("x", 425)
+    .attr("y", 100)
+    .attr("position","absolute");
+
+    form.append("input")
+    .attr("id", "slider")
+    .attr("type","text")
+    .attr("x", 0)
+    .attr("y", 0);
+
+    form.append("div")
+    .attr("id", "percentage")
+    .attr("x", 0)
+    .attr("y", 0);
+
+    if(d.percent){
+       addSlider(d.percent);
+    }else{
+       addSlider(0);
+    }
+
+    addWheel();
+    if(selectedData.color){
+       colorWheel.color(selectedData.color);
+    }else{
+       colorWheel.color(d3.select("#" + ID_PRFX_Task + selectedData.id).select("rect").attr("fill"));
+    }
 }
 
 function myAdd(d){
@@ -364,11 +453,23 @@ function myAdd(d){
 }
 
 function myEdit(d){
-	zoomTo(d, 750);
-	setTimeout(function(){showForm(d);}, 760);
+	if(!formVisible){
+		zoomTo(d, 750, true);
+		setTimeout(function(){
+		        d3.selectAll("text").attr("display","none");
+			showForm(d);
+			}, 760);
+	}
+	formVisible = true;
 }
 
 function myDelete(d){
+	var menu = d3.select("#" + ID_Menu);
+	if(menu){
+		menu.remove();
+		formVisible = false;
+	}
+
 	// repair parent pointers
 	var siblings = d.parent.children;
 	if(d.children){
@@ -387,11 +488,14 @@ function myDelete(d){
 }
 
 function createCanvas(){
-	var localCanvas = d3.select("#taskArea").append("svg")
-		.attr("width", WIDTH_Canvas)
-		.attr("height", HEIGHT_Canvas)
-		.attr("id", ID_Canvas)
-		.attr("display", "block");
+	var localCanvas = d3.select("body").append("svg")
+	.style("position", "absolute")
+	.style("left", PAD_Buttons + "px")
+	.style("top", HEIGHT_Buttons+PAD_Buttons+"px")
+	.attr("width", WIDTH_Canvas)
+	.attr("height", HEIGHT_Canvas)
+	.attr("id", ID_Canvas)
+	.attr("display", "block");
 	return localCanvas;
 }
 
@@ -407,8 +511,28 @@ $(document).ready(function(){
 	  drawTree(canvas, partition(taskTree));
 	  updateSelectedData(taskTree);
 	});
+	$("#jsonOverRide").click(function(){
+		var newJson;
+                var isJSON = true;
+                try{
+			newJson = jQuery.parseJSON($("#jsonArea").val());
+		}
+		catch(err){
+			alert("Error: That is not a JSON object!")
+                        console.log("Parsing TEXT: ",newJson);
+                        console.log("Parsing Error: ",err);
+			isJSON = false;
+		}
+		if(isJSON){
+			try{
+				drawTree(canvas,partition(newJson));
+				taskTree = jQuery.parseJSON($("#jsonArea").val());
+			}
+			catch(err){
+				alert("Error: That is an invalid JSON Object!");
+	                        console.log("ATTEMPTED TO PARTITION: ",newJson);
+                                console.log("PARTITION ERROR: ",err);
+			}
+		}
+	});
 });
-
-
-
-
